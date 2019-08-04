2e14dc1bf48a3e9356daa37e11b33964669ec93a
Denis Tolmachev <oovilden@gmail.com>
2019-08-05 01:03:56 +0500

    fix spinner animation

--- src/components/WithSpinner/WithSpinner.styles.js
+++ src/components/WithSpinner/WithSpinner.styles.js
@@ -10,21 +10,16 @@ export const SpinnerOverlay = styled.div`
 
 export const SpinnerContainer = styled.div`
   animation: spin 1s ease-in-out infinite;
+  border: 3px solid rgba(195, 195, 195, 0.6);
   border-radius: 50%;
   border-top-color: #636767;
-  border: 3px solid rgba(195, 195, 195, 0.6);
   display: inline-block;
   height: 50px;
   width: 50px;
-  -webkit-animation: spin 1s ease-in-out infinite;
+
   @keyframes spin {
     to {
-      -webkit-transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes spin {
-    to {
-      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
     }
   }
 `;
