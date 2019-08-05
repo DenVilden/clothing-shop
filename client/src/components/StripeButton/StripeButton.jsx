1de90b08fb75afd190c4eee9c7b30ea7a3590371
Denis Tolmachev <oovilden@gmail.com>
2019-08-06 00:53:33 +0500

    added server to handle stripe payment

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 import image from '../../assets/CUz.svg';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_Htdm0Mcd18IgI0qJfYco6stX00ZwuoWXfv';
 
+  /* eslint-disable no-alert */
   const onToken = token => {
-    // eslint-disable-next-line no-console
-    console.log(token);
-    // eslint-disable-next-line no-alert
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: { amount: priceForStripe, token }
+    })
+      .then(() => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.log('Payment error:', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use provided credit card'
+        );
+      });
   };
 
   return (
