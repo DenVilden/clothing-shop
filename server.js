870653ddbdf99abdd8de0d948498c033a2218316
Denis Tolmachev <oovilden@gmail.com>
2019-08-06 18:32:43 +0500

    update server path

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const stripe = require('stripe');
-const path = require('path');
-
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,32 +12,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile('client/build/index.html');
   });
+} else {
+  dotenv.config();
 }
-
-app.listen(port, error => {
-  if (error) throw error;
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${port}`);
-});
-
-const stripeApi = stripe(process.env.STRIPE_SECRET_KEY);
 
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd',
+    currency: 'usd'
   };
 
-  stripeApi.charges.create(body, (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      res.status(500).send({ error: stripeErr });
-    } else {
-      res.status(200).send({ success: stripeRes });
+  stripe(process.env.STRIPE_SECRET_KEY).charges.create(
+    body,
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).send({ error: stripeErr });
+      } else {
+        res.status(200).send({ success: stripeRes });
+      }
     }
-  });
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
