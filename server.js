7fcf1bb99c9ed0c44491528de3a62b4e274e1e2d
Denis Tolmachev <oovilden@gmail.com>
2019-08-15 15:25:47 +0500

    add gziping on server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const stripe = require('stripe');
-const path = require('path');
+const compression = require('compression');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
