899134de5e473133a3f8492a29ecd41e937243a1
Denis Tolmachev <oovilden@gmail.com>
2019-08-16 10:27:12 +0500

    optimize images

--- src/reducers/directory.reducer.js
+++ src/reducers/directory.reducer.js
@@ -1,33 +1,39 @@
+import hats from '../assets/img/hats.webp';
+import jackets from '../assets/img/jackets.webp';
+import sneakers from '../assets/img/sneakers.webp';
+import womens from '../assets/img/womens.webp';
+import men from '../assets/img/men.webp';
+
 const initialState = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: hats,
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: jackets,
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: sneakers,
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: womens,
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: men,
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
