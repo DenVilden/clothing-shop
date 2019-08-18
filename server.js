b99eb9daa6cbcee00fd81ea88c675cb7826d4d39
Denis Tolmachev <oovilden@gmail.com>
2019-08-18 14:07:15 +0500

    remove body-parser and cors

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const stripe = require('stripe');
 const compression = require('compression');
@@ -10,9 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
