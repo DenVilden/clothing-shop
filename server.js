75fa0d119819d502f2508a9f50057dbf067580d8
Denis Tolmachev <oovilden@gmail.com>
2019-08-18 15:04:38 +0500

    add http to https redirect

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const stripe = require('stripe');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
