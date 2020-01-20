fcd18fb891e207805e8c2d1887a281f64525cf36
Denis Tolmachev <oovilden@gmail.com>
2020-01-20 20:17:05 +0500

    fix styling bug: missed semicolon

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -41,33 +41,33 @@ export default withRouter(MenuItem);
 
 /* STYLES */
 const MenuItemContainer = styled.div`
-	align-items: center;
-	border: 1px solid black;
-	display: flex;
-	flex: 1 1 auto;
-	height: ${({ size }) => (size ? '380px' : '240px')}
-	justify-content: center;
-	margin: 0 7.5px 15px;
-	min-width: 30%;
-	overflow: hidden;
+  align-items: center;
+  border: 1px solid black;
+  display: flex;
+  flex: 1 1 auto;
+  height: ${({ size }) => (size ? '380px' : '240px')};
+  justify-content: center;
+  margin: 0 7.5px 15px;
+  min-width: 30%;
+  overflow: hidden;
 
   @media (max-width: 800px) {
     height: 200px;
-  } 
-  
-	&:hover {
-    cursor: pointer;
-    
-		& .background-image {
-			transform: scale(1.1);
-			transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-		}
-		& .content {
-			opacity: 0.9;
-		}
   }
-  
-	&:first-child {
+
+  &:hover {
+    cursor: pointer;
+
+    & .background-image {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+    & .content {
+      opacity: 0.9;
+    }
+  }
+
+  &:first-child {
     margin-right: 7.5px;
   }
 
